f7c2febddd985334b30f23353478d40b24b03a9f
student <tshiknn@gmail.com>
2025-10-14 20:03:31 -0700

    Implementation first draft.

--- dgemm-basic-omp.cpp
+++ dgemm-basic-omp.cpp
@@ -1,5 +1,7 @@
 #include <iostream>
 #include <omp.h>
+
+#define LIKWID_PERFMON  // TODO: comment this out
 #include "likwid-stuff.h"
 
 const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm.";
@@ -12,11 +14,28 @@ const char* dgemm_desc = "Basic implementation, OpenMP-enabled, three-loop dgemm
  */
 void square_dgemm(int n, double* A, double* B, double* C) 
 {
-   // insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
+    // insert your code here: implementation of basic matrix multiply with OpenMP parallelism enabled
 
-   // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
-   // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
-   // after the matrix multiply code but before the end of the parallel code block.
+    // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
+    // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
+    // after the matrix multiply code but before the end of the parallel code block.
 
-   std::cout << "Insert your basic matrix multiply, openmp-parallel edition here " << std::endl;
+#pragma omp parallel
+    {
+        LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
+
+#pragma omp for collapse(2)
+        for (int i = 0; i < n; i++) { // rows
+            for (int j = 0; j < n; j++) { // columns
+                double square_sum = C[i*n + j];
+                for (int k = 0; k < n; k++) {
+                    // C[i, j] = C[i, j] + A[i, n] * B[n, j];
+                    square_sum += A[i*n + k] * B[k*n + j];
+                }
+                C[i*n + j] = square_sum;
+            }
+        }
+
+        LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+    }
 }
--- dgemm-blocked-omp.cpp
+++ dgemm-blocked-omp.cpp
@@ -2,10 +2,42 @@
 #include <string.h>
 #include <stdlib.h>
 #include <omp.h>
+#include <vector>
+#include <cstring>
+
+#define LIKWID_PERFMON
 #include "likwid-stuff.h"
 
 const char* dgemm_desc = "Blocked dgemm, OpenMP-enabled";
 
+void square_dgemm_basic(int n, double* A, double* B, double* C) {
+    for (int i = 0; i < n; i++) { // rows
+        for (int j = 0; j < n; j++) { // columns
+            double square_sum = C[i*n + j];
+            for (int k = 0; k < n; k++) {
+                // C[i, j] = C[i, j] + A[i, n] * B[n, j];
+                square_sum += A[i*n + k] * B[k*n + j];
+            }
+            C[i*n + j] = square_sum;
+        }
+    }
+}
+
+void copy_block(double *dest, double *src, int n, int block_size) {
+    for (int y = 0; y < block_size; y++) {
+        std::memcpy(&dest[y * block_size],
+            &src[y * n],
+            block_size * sizeof(double));
+    }
+}
+
+void write_block(double *dest, double *src, int n, int block_size) {
+    for (int y = 0; y < block_size; y++) {
+        std::memcpy(&dest[y * n],
+            &src[y * block_size],
+            block_size * sizeof(double));
+    }
+}
 
 /* This routine performs a dgemm operation
  *  C := C + A * B
@@ -13,11 +45,38 @@ const char* dgemm_desc = "Blocked dgemm, OpenMP-enabled";
  * On exit, A and B maintain their input values. */
 void square_dgemm_blocked(int n, int block_size, double* A, double* B, double* C) 
 {
-   // insert your code here: implementation of blocked matrix multiply with copy optimization and OpenMP parallelism enabled
+    const int Nb = n / block_size;
+    const int block_arr_size = block_size * block_size;
 
-   // be sure to include LIKWID_MARKER_START(MY_MARKER_REGION_NAME) inside the block of parallel code,
-   // but before your matrix multiply code, and then include LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME)
-   // after the matrix multiply code but before the end of the parallel code block.
+#pragma omp parallel
+    {
+        LIKWID_MARKER_START(MY_MARKER_REGION_NAME);
 
-   std::cout << "Insert your blocked matrix multiply with copy optimization, openmp-parallel edition here " << std::endl;
+        double* buf_mem = new double[block_arr_size * 3];
+        double* An = &buf_mem[0];
+        double* Bn = &buf_mem[block_arr_size];
+        double* Cn = &buf_mem[block_arr_size * 2];
+
+#pragma omp for collapse(2)
+        for (int i = 0; i < Nb; i++) {   // row
+            for (int j = 0; j < Nb; j++) {   // col
+                // calculate this so we store it in register
+                double* Cpos = &C[i * n * block_size + j * block_size];
+                copy_block(Cn, Cpos, n, block_size);
+
+                for (int k = 0; k < Nb; k++) {
+                    copy_block(An, &A[i * n * block_size + k * block_size], n, block_size);
+                    copy_block(Bn, &B[k * n * block_size + j * block_size], n, block_size);
+
+                    square_dgemm_basic(block_size, An, Bn, Cn);
+                }
+
+                write_block(Cpos, Cn, n, block_size);
+            }
+        }
+
+        delete[] buf_mem;
+
+        LIKWID_MARKER_STOP(MY_MARKER_REGION_NAME);
+    }
 }
